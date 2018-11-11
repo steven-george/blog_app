121d266ffa60a75962ced658f1009243752429b3
Steven George <GEORGE@Stevens-MBP.lan>
2018-11-11 16:43:31 +0100

    First commit

---
+++ vir_env_blog_app/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/GEORGE/Desktop/Code/DJANGO 2 BY EXAMPLE/BLOG APP/vir_env_blog_app/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
